0c40a15937896b64f8847647fee95c4ff2254bc4
juju9595 <git>
2025-09-01 16:26:23 +0900

    20250901 집에서 완료

--- src/main/java/example/실습/실습1/TaskDao.java
+++ src/main/java/example/실습/실습1/TaskDao.java
@@ -1,32 +1,59 @@
 package example.실습.실습1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
 
 @Repository
 public class TaskDao{
-    private String db_url = "jdbc:mysql://localhost:3306/springweb2";
-    private String db_user = "root";
-    private String db_password = "1234";
-    public Connection conn;
-    public TaskDao(){connect();}
-    private void connect(){
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(db_url, db_user, db_password);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    //3.
-    public void task3(int product_id){
-        try {
-            String sql = "uqdate products set 10 >= stock_quantity = stock_quantity + 20 where product_id";
-            PreparedStatement ps = conn.prepareStatement(sql);
+    @Autowired
+    private DataSource dataSource;
 
+    //1. 모든 제품 3개씩 감소 요청
+    public int task1(){
+        String sql = "update products set stock_quantity - 3 where stock_quantity >= 3";
+        try(
+            Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+            int result = ps.executeUpdate();  //업데이트된 갯수
+            return result;
+        }catch (Exception e){
+            System.out.println(e);
+        }return 0;
+    }
+
+
+    //2. 모든 제품 정보를 조회/출력 한다.
+    public List<Map<String, Object>> task2(){
+        List<Map<String, Object>> list = new ArrayList<>();
+        String sql = "select *from products";
+        try(Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("product_id", rs.getInt("product_id"));
+                map.put("product_name", rs.getString("product_name"));
+                map.put("stock_quantity", rs.getInt("stock_quantity"));
+                list.add(map);
+            }
+        } catch (Exception e) {System.out.println(e);}
+    return list;
+
+    }
+
+    //3.
+    public void task3(){
+        String sql = "uqdate products set stock_quantity = stock_quantity + 20 where stock_quantity <= 10";
+        try {Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            int result = ps.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
         }
--- src/main/java/example/실습/실습1/TaskService.java
+++ src/main/java/example/실습/실습1/TaskService.java
@@ -1,5 +1,6 @@
 package example.실습.실습1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +10,15 @@ import java.util.Map;
 @Service
 public class TaskService {
 
+    @Autowired
+    private TaskDao taskDao;
+
        // 1. 매 30초마다 모든 제품의 재고는 3개씩 감소한다.
     @Scheduled( cron = " 0/30 * * * * *") //d예시] 12:57:00 , 12:57:30
     public void task1(){
             System.out.println("TaskService.task1");
             // DAO 호출하여 모든 제품 3개씩 감소 요청/응답++
+            taskDao.task1();
 
         }
 
@@ -23,6 +28,7 @@ public class TaskService {
     public void task2() {
         System.out.println("TaskService.task2");
         //모든 제품 정보를 조회/출력 한다.
+        taskDao.task2();
     }
 
 
@@ -31,6 +37,7 @@ public class TaskService {
     public void task3(){
         System.out.println("TaskService.task3");
         //재고가 10 이하인 상품의 재고를 +20개 추가한다.
+        taskDao.task3();
     }
 
 
