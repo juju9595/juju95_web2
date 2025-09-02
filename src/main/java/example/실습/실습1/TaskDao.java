45db6fab8f60964fd7e6bdb9d6adce23457a89c8
juju9595 <eunju220926@gmail.com>
2025-09-02 17:09:12 +0900

    0902 오후 5시

--- src/main/java/example/실습/실습1/TaskDao.java
+++ src/main/java/example/실습/실습1/TaskDao.java
@@ -11,17 +11,14 @@ import java.sql.ResultSet;
 import java.util.*;
 
 @Repository
-public class TaskDao{
+public class TaskDao extends Dao{
 
-    @Autowired
-    private DataSource dataSource;
 
     //1. 모든 제품 3개씩 감소 요청
     public int task1(){
-        String sql = "update products set stock_quantity - 3 where stock_quantity >= 3";
-        try(
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql)){
+        try{
+            String sql = "update products set stock_quantity - 3 where stock_quantity >= 3";
+            PreparedStatement ps = conn.prepareStatement(sql);
             int result = ps.executeUpdate();  //업데이트된 갯수
             return result;
         }catch (Exception e){
@@ -33,29 +30,26 @@ public class TaskDao{
     //2. 모든 제품 정보를 조회/출력 한다.
     public List<Map<String, Object>> task2(){
         List<Map<String, Object>> list = new ArrayList<>();
-        String sql = "select *from products";
-        try(Connection conn = dataSource.getConnection();
-        PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery()) {
+        try{
+            String sql = "select *from products";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 Map<String, Object> map = new HashMap<>();
-                map.put("product_id", rs.getInt("product_id"));
                 map.put("product_name", rs.getString("product_name"));
                 map.put("stock_quantity", rs.getInt("stock_quantity"));
                 list.add(map);
             }
-        } catch (Exception e) {System.out.println(e);}
-    return list;
-
-
+        } catch (Exception e) {System.out.println(e);
+        }return list;
     }
 
-    //3.
+    //3. 재고가 10 이하인 상품의 재고를 +20개 추가한다.
     public void task3(){
-        String sql = "uqdate products set stock_quantity = stock_quantity + 20 where stock_quantity <= 10";
-        try {Connection conn = dataSource.getConnection();
+        try {
+            String sql = "uqdate products set stock_quantity = stock_quantity + 20 where stock_quantity <= 10";
             PreparedStatement ps = conn.prepareStatement(sql);
-            int result = ps.executeUpdate();
+            ps.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
         }
