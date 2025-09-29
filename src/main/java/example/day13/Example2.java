c40b6d39a5128d68d4a1d6d155cf61b7d26ad09f
juju9595 <eunju220926@gmail.com>
2025-09-29 18:20:11 +0900

    0929 오후 06시 30분

--- src/main/java/example/day13/Example2.java
+++ src/main/java/example/day13/Example2.java
@@ -1,5 +1,6 @@
 package example.day13;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +22,47 @@ public class Example2 {
         List< Integer > newNumbers =
             numbers.stream().map( x ->  x * 2 ).collect( Collectors.toList());
         System.out.println("[2] 새로운 리스트 : " + newNumbers);
+
+        // [3] stream() + map() + 최종출력=forEach()
+        numbers.stream()                    // 스트림 시작
+                .map( x -> x * 2)   // 중간 연산
+                .forEach( x -> System.out.println("[3] map+foreach : " + x));   // 최종 출력
+
+        // [4] stream() + filter() + 최종출력
+        numbers.stream()                            // 스트림 시작
+                .filter( x -> x % 2 == 0)   // 중간연산 : 짝수만
+                .forEach( x -> System.out.println("[4] filter+foreach : " + x)); //최종출력
+
+        // [5] stream() + sorted() + 최종출력
+                numbers.stream()
+                        //.sorted()//기본값 : 오름차순
+                        .sorted(Comparator.reverseOrder()) // 내림차순
+                        .forEach( x -> System.out.println("[5] sorted+foreach : " + x));
+
+        // [6] stream() + distinct() + 최종출력 , 중복제거
+        List<Integer> disList = numbers.stream().distinct().collect( Collectors.toList());
+        System.out.println("[6] distinct : " + disList);
+
+        //[7] stream() + limit(N) + 최종출력, 처음부터 N개의 데이터만 제한
+        numbers.stream()
+                .limit(5)
+                .forEach(x -> System.out.println( x ));
+
+        //[8-1] stream() + reduce(초기값, (누적값, 현재값) -> 연산)
+        int sum = numbers.stream().reduce(0, (누적값, 현재값) -> 누적값 + 현재값);
+        System.out.println("[8-1] sum : " + sum);
+
+        //[8-2]
+        int product = numbers.stream().reduce(1, (누적값, 현재값) -> 누적값 * 현재값);
+        System.out.println("[8-2] product : " + product);
+
+        //[8-3]
+        int min = numbers.stream()
+                .reduce( Integer.MAX_VALUE , (앞전값, 현재값) -> 앞전값 < 현재값 ? 앞전값 : 현재값);
+        System.out.println("[8-3] min :" + min );
+        int max = numbers.stream()
+                .reduce(0, (a,b) -> a > b ? a : b);
+        System.out.println("[8-4] max : " + max);
 
     }//main e
 }//class e
